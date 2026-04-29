dc03e1e39f2441775dcc7b9fd8b559758a7a5bc4
Umezawa <s25g1021bw@s.chibakoudai.jp>
2026-04-29 13:42:53 +0900

    ハッカソン追加

---
+++ work/umezawa/hck/week2/HCK02_01/HCK02_01/HCK02_01.ino
@@ -0,0 +1,26 @@
+#define BAUD 921600 // ボーレート（速め）
+#define PIN 0 // A0 アナログ入⼒
+#define RESOLUTION 10 // 量⼦化10bit
+
+void setup() {
+  // マイクのポートを指定
+  pinMode(PIN, INPUT);
+  // シリアル通信の速度を設定(bit per second)
+  Serial.begin(BAUD);
+  // アナログ読み込みの量⼦化精度
+  analogReadResolution(RESOLUTION);
+}
+
+void loop() {
+  // A0から読み込み
+  int d = analogRead(PIN);
+  // 読み込んだ値を量⼦化精度で規格化し，電圧を取得
+  float a = (float) d / (pow(2, RESOLUTION)-1)*5.0;
+  float maxa = 3.3/2.0; // 振幅最⼤値
+  a = a - maxa; // 中⼼を0にする
+  float mina = -maxa; // 振幅最⼩値
+
+  // シリアルモニタに出⼒
+  Serial.println(a);
+  
+}
